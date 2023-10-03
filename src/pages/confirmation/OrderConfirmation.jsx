c905f123ddf3e249aab78baafbd8dda216c2f7e8
NahyeonKim <parakise13@gmail.com>
2023-10-03 12:35:53 +0900

    modified orderconfirmation

--- src/pages/confirmation/OrderConfirmation.jsx
+++ src/pages/confirmation/OrderConfirmation.jsx
@@ -2,10 +2,12 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Button from "react-bootstrap/Button";
 import { useOrderDetails } from "../../contexts/OrderDetails";
+import AlertBanner from "../common/AlertBanner";
 
 export default function OrderConfirmation({ setOrderPhase }) {
     const { resetOrder } = useOrderDetails();
     const [orderNumber, setOrderNumber] = useState(null);
+    const [error, setError] = useState(false);
 
     useEffect(() => {
         axios
@@ -15,10 +17,21 @@ export default function OrderConfirmation({ setOrderPhase }) {
             .then((response) => {
                 setOrderNumber(response.data.orderNumber);
             })
-            .catch((error) => {
-                // TODO: handle error from server
-            });
+            .catch((error) => setError(true));
     }, []);
+
+    const newOrderButton = (
+        <Button onClick={handleClick}>Create new order</Button>
+    );
+
+    if (error) {
+        return (
+            <>
+                <AlertBanner message={null} variant={null} />
+                {newOrderButton}
+            </>
+        );
+    }
 
     function handleClick() {
         // clear the order details
@@ -28,14 +41,18 @@ export default function OrderConfirmation({ setOrderPhase }) {
         setOrderPhase("inProgress");
     }
 
-    return (
-        <div style={{ textAlign: "center" }}>
-            <h1>Thank You!</h1>
-            <p>Your order number is {orderNumber}</p>
-            <p style={{ fontSize: "25%" }}>
-                as per our terms and conditions, nothing will happen now
-            </p>
-            <Button onClick={handleClick}>Create new order</Button>
-        </div>
-    );
+    if (orderNumber) {
+        return (
+            <div style={{ textAlign: "center" }}>
+                <h1>Thank You!</h1>
+                <p>Your order number is {orderNumber}</p>
+                <p style={{ fontSize: "25%" }}>
+                    as per our terms and conditions, nothing will happen now
+                </p>
+                {newOrderButton}
+            </div>
+        );
+    } else {
+        return <div>Loading</div>;
+    }
 }
